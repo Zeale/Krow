2fca47649e653bce77c5d87e1593b7d232c9a076
Zeale <a@infinityminers.com>
2017-05-21 08:51:02 -0500

    Documented a few files

--- src/kröw/zeale/v1/program/core/OldVersionLoader.java
+++ src/kröw/zeale/v1/program/core/OldVersionLoader.java
@@ -7,17 +7,49 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectStreamClass;
+import java.io.ObjectStreamException;
 import java.io.OutputStream;
 
 import wolf.mindset.Construct;
 import wolf.mindset.Law;
+import wolf.mindset.MindsetObject;
 
+/**
+ * An internal object used by the {@link Kröw} program to backwards-compatibly
+ * load objects from previous versions of {@link Kröw}. This may be
+ * removed/changed at any time, so it should not be used externally.
+ *
+ * @author Zeale
+ * @internal This class is internal.
+ *
+ */
 final class OldVersionLoader {
 
+	/**
+	 * Prevent instantiation of this class.
+	 */
 	private OldVersionLoader() {
 
 	}
 
+	/**
+	 * Returns an {@link ObjectInputStream} which can be used to load previous
+	 * versions of {@link MindsetObject}s or current versions. The loaded
+	 * objects will belong to the current version after they are loaded,
+	 * regardless of how they were saved.
+	 *
+	 * @param file
+	 *            The {@link File} to build the {@link ObjectInputStream} with.
+	 * @return A new {@link ObjectStreamException} which can
+	 *         backwards-compatibly load {@link MindsetObject}s. The stream will
+	 *         be built using the specified {@link File}.
+	 * @throws FileNotFoundException
+	 *             As specified by
+	 *             {@link FileInputStream#FileInputStream(File)}.
+	 * @throws IOException
+	 *             As specified by
+	 *             {@link java.io.ObjectInputStream#ObjectInputStream(InputStream)}.
+	 */
 	public static ObjectInputStream getInputStream(final File file) throws FileNotFoundException, IOException {
 		return new ObjectInputStream(new FileInputStream(file));
 	}
