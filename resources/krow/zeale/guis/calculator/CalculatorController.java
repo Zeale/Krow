5c2b23efe1f01bd5aa901096d8f336e68fdc4a93
Zeale <a@infinityminers.com>
2017-06-21 20:34:04 -0500

    Added calculator files

---
+++ resources/krow/zeale/guis/calculator/CalculatorController.java
@@ -0,0 +1,14 @@
+package krow.zeale.guis.calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * The controller for the calculator window.
+ * 
+ * @author Zeale
+ *
+ */
+public class CalculatorController {
+	@FXML private TextField outputField;
+}
