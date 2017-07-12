9c4659da5fa44675d0d955f9808c8c9ea23b828e
Zeale <a@infinityminers.com>
2017-07-12 01:13:09 -0500

    Added Settings file

---
+++ src/krow/kröw/core/Settings.java
@@ -0,0 +1,12 @@
+package kröw.core;
+
+import java.io.Serializable;
+
+public class Settings implements Serializable {
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
