5fb38c433cf0c8308df261f72bfdfe18150ebd3d
Zeale <a@infinityminers.com>
2017-07-26 12:27:16 -0500

    Updated Settings window

--- src/guis/zeale/guis/Settings.java
+++ src/guis/zeale/guis/Settings.java
@@ -1,9 +1,15 @@
 package zeale.guis;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import kröw.core.Kröw;
 import kröw.libs.guis.Window;
 
 public class Settings extends Window {
+
+	@FXML
+	private Label labelSettings;
 
 	@Override
 	public String getWindowFile() {
@@ -14,6 +20,8 @@ public class Settings extends Window {
 	public void initialize() {
 		Window.getStage().getScene().setFill(Color.TRANSPARENT);
 		Window.getStage().setFullScreen(true);
+		labelSettings.setLayoutX((double) 265 / 1920 * Kröw.SCREEN_WIDTH);
+		labelSettings.setLayoutY((double) 160 / 1080 * Kröw.SCREEN_HEIGHT);
 	}
 
 }
