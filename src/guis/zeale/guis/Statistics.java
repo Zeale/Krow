d4eb467d8bb7fe91bf69cf25e2b33c0012bfffdb
Zeale <a@infinityminers.com>
2017-08-24 14:48:20 -0500

    Updated Statistics.java

--- src/guis/zeale/guis/Statistics.java
+++ src/guis/zeale/guis/Statistics.java
@@ -1,9 +1,16 @@
 package zeale.guis;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
 import krow.guis.GUIHelper;
 import kröw.core.Kröw;
 import kröw.core.managers.WindowManager;
@@ -18,6 +25,8 @@ public class Statistics extends WindowManager.Page {
 
 	@FXML
 	private ListView<Object> searchList;
+
+	private static final ObservableList<Object> searchItems = FXCollections.observableArrayList();
 
 	private static final double SEARCH_BAR_WIDTH = 1241, SEARCH_BAR_HEIGHT = 44;
 	private static final double SEARCH_BAR_LAYOUT_X = 340, SEARCH_BAR_LAYOUT_Y = 24;
@@ -39,7 +48,90 @@ public class Statistics extends WindowManager.Page {
 		searchList.setLayoutX(Kröw.scaleWidth(SEARCH_LIST_LAYOUT_X));
 		searchList.setLayoutY(Kröw.scaleHeight(SEARCH_LIST_LAYOUT_Y));
 
+		searchList.setItems(searchItems);
+
+		searchList.setCellFactory(new Callback<ListView<Object>, ListCell<Object>>() {
+
+			@Override
+			public ListCell<Object> call(ListView<Object> param) {
+				return null;
+			}
+		});
+
 		GUIHelper.applyShapeBackground(pane, searchBar, searchList);
+	}
+
+	public static class Statistic {
+
+		private String stat, val;
+
+		private ListCell<Object> cell;
+
+		/**
+		 * @return the cell
+		 */
+		public final ListCell<Object> getCell() {
+			return cell;
+		}
+
+		/**
+		 * @param cell
+		 *            the cell to set
+		 */
+		public final void setCell(ListCell<Object> cell) {
+			this.cell = cell;
+		}
+
+		/**
+		 * @return the stat
+		 */
+		public final String getStat() {
+			return stat;
+		}
+
+		/**
+		 * @param stat
+		 *            the stat to set
+		 */
+		public final void setStat(String stat) {
+			updateCell();
+			this.stat = stat;
+		}
+
+		/**
+		 * @return the val
+		 */
+		public final String getVal() {
+			return val;
+		}
+
+		/**
+		 * @param val
+		 *            the val to set
+		 */
+		public final void setVal(String val) {
+			updateCell();
+			this.val = val;
+		}
+
+		protected void updateCell() {
+			if (isAssignedToCell())
+				cell.setItem(this);
+		}
+
+		public boolean isAssignedToCell() {
+			return cell != null;
+		}
+
+	}
+
+	public static class AutoUpdatingStatistic extends Statistic {
+		private static final Thread updater = new Thread();
+		private static Collection<AutoUpdatingStatistic> statistics = new ArrayList<>();
+	}
+
+	public void addStatistic(Statistic statistic) {
+		searchItems.add(statistic);
 	}
 
 	@Override
