b79d295d2e29632da82a149094f2cacf6810ec89
Zeale <a@infinityminers.com>
2017-08-28 02:18:50 -0500

    Updated ChatRoom

--- src/guis/zeale/guis/ChatRoom.java
+++ src/guis/zeale/guis/ChatRoom.java
@@ -1,12 +1,36 @@
 package zeale.guis;
 
+import java.net.Socket;
+import java.util.Stack;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextFlow;
+import krow.guis.GUIHelper;
 import kröw.core.managers.WindowManager;
 
 public class ChatRoom extends WindowManager.Page {
+
+	private Stack<String> messageQueue = new Stack<>();
+	private Socket socket;
+
+	private Thread sendThread = new Thread(new Runnable() {
+
+		@Override
+		public void run() {
+			while (!messageQueue.isEmpty()) {
+
+			}
+		}
+	});
 
 	@Override
 	public String getWindowFile() {
@@ -20,5 +44,58 @@ public class ChatRoom extends WindowManager.Page {
 
 	@FXML
 	private AnchorPane pane;
+	@FXML
+	private Button sendButton;
+
+	private void emptyMessageWarning() {
+		WindowManager.spawnLabelAtMousePos("Empty message...", Color.FIREBRICK);
+	}
+
+	private void sendingMessageNotification() {
+		WindowManager.spawnLabelAtMousePos("Sending...", Color.GREEN);
+	}
+
+	@Override
+	public void initialize() {
+		sendButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				if (!chatBox.getText().isEmpty()) {
+					sendMessage(chatBox.getText());
+					sendingMessageNotification();
+				} else
+					emptyMessageWarning();
+			}
+		});
+
+		chatBox.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode().equals(KeyCode.ENTER)) {
+					if (event.isShiftDown() ^ event.isControlDown())
+						chatBox.appendText("\n");
+					if (!event.isShiftDown()) {
+						event.consume();
+						if (!chatBox.getText().isEmpty()) {
+							sendMessage(chatBox.getText());
+							sendingMessageNotification();
+						} else
+							emptyMessageWarning();
+					}
+				}
+			}
+		});
+
+		GUIHelper.addDefaultSettings(GUIHelper.buildMenu(pane));
+		GUIHelper.applyShapeBackground(pane, chatPane, chatBox);
+	}
+
+	private void sendMessage(String message) {
+		chatBox.setText("");
+		chatPane.getChildren().add(new Label(message));
+		// TODO Implement
+	}
 
 }
