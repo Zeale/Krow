0ad20d2b71f596ebc7f878392e5e68867f66b643
Zeale <a@infinityminers.com>
2017-09-04 01:24:38 -0500

    Added project files (Game Hub)

---
+++ src/guis/zeale/guis/gamehub/GameHub.java
@@ -0,0 +1,12 @@
+package zeale.guis.gamehub;
+
+import kröw.core.managers.WindowManager.Page;
+
+public class GameHub extends Page {
+
+	@Override
+	public String getWindowFile() {
+		return "GameHub.fxml";
+	}
+
+}
