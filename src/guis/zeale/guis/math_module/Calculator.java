84fcebd6e22d143dba034ac0d3f6b62a5e15700e
Zeale <a@infinityminers.com>
2017-09-08 23:44:58 -0500

    Added Calculator files

---
+++ src/guis/zeale/guis/math_module/Calculator.java
@@ -0,0 +1,12 @@
+package zeale.guis.math_module;
+
+import kröw.core.managers.WindowManager.Page;
+
+public class Calculator extends Page {
+
+	@Override
+	public String getWindowFile() {
+		return "Calculator.fxml";
+	}
+
+}
