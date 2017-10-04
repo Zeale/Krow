d032c0985c46fe78d94b05e796e02605654fc430
Zeale <a@infinityminers.com>
2017-10-03 20:56:32 -0500

    Modified Popup API

--- src/guis/krow/guis/Popup.java
+++ src/guis/krow/guis/Popup.java
@@ -1,13 +1,17 @@
 package krow.guis;
 
 import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 import kröw.core.managers.WindowManager;
 
 public class Popup extends javafx.stage.Popup {
@@ -40,6 +44,21 @@ public class Popup extends javafx.stage.Popup {
 
 		bindeNode(node, hover ? MouseEvent.MOUSE_ENTERED : MouseEvent.MOUSE_CLICKED,
 				hover ? mouseEntered : mouseClicked);
+	}
+
+	public Popup(Node node, String text) {
+		FadeTransition openingAnimation = new FadeTransition(Duration.millis(2000), getRoot()),
+				closingAnimation = new FadeTransition(Duration.millis(2000), getRoot());
+		openingAnimation.setToValue(1);
+		closingAnimation.setToValue(0);
+		getRoot().setOpacity(0);
+		this.openingAnimation = openingAnimation;
+		this.closingAnimation = closingAnimation;
+
+		Label lbl = new Label(text);
+		lbl.setTextFill(Color.WHITE);
+		((AnchorPane) getRoot()).getChildren().add(lbl);
+		bindNode(node);
 	}
 
 	public void bindNode(Node node) {
@@ -104,17 +123,15 @@ public class Popup extends javafx.stage.Popup {
 	}
 
 	public void open() {
+		show(WindowManager.getStage());
 		if (openingAnimation != null)
 			openingAnimation.play();
-		else
-			show(WindowManager.getStage());
 	}
 
 	public void close() {
 		if (closingAnimation != null)
 			closingAnimation.play();
-		else
-			hide();
+		hide();
 	}
 
 }
