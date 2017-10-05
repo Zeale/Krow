e8197a0c2224a116d75e373637a02ab718b66801
Zeale <a@infinityminers.com>
2017-10-05 15:03:49 -0500

    Updated Popup API

--- src/guis/krow/guis/PopupHelper.java
+++ src/guis/krow/guis/PopupHelper.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.InnerShadow;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -133,6 +133,85 @@ public final class PopupHelper {
 
 		applyPopup(boundNode, popup);
 		return box;
+	}
+
+	public static void applyRightClickPopup(Node node, Popup popup) {
+		Parent popupRoot = popup.getScene().getRoot();
+		FadeTransition openTransition = new FadeTransition(Duration.millis(350), popupRoot),
+				closeTransition = new FadeTransition(Duration.millis(350), popupRoot);
+		openTransition.setToValue(1);
+		closeTransition.setToValue(0);
+		popupRoot.setOpacity(0);
+	
+		closeTransition.setOnFinished(event -> popup.hide());
+	
+		new Object() {
+	
+			private byte prevEvent = -1;
+	
+			{
+	
+				node.addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
+					if (!popup.isShowing())
+						return;
+					prevEvent = 0;
+					open(event);
+				});
+	
+				node.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+	
+					@Override
+					public void handle(MouseEvent event) {
+						if (event.getButton().equals(MouseButton.SECONDARY)) {
+							event.consume();
+							open(event);
+						}
+					}
+				});
+	
+				node.addEventFilter(MouseEvent.MOUSE_EXITED, event -> {
+					// Make sure we aren't leaving and going to this node.
+					if (event.getPickResult().getIntersectedNode() == node)
+						return;
+	
+					prevEvent = 1;
+					close();
+				});
+	
+				popupRoot.addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
+					prevEvent = 2;
+					open(event);
+				});
+	
+				popupRoot.addEventFilter(MouseEvent.MOUSE_EXITED, event -> {
+					if (prevEvent == 0)
+						return;
+					prevEvent = 3;
+					close();
+				});
+	
+			}
+	
+			private void open(MouseEvent event) {
+				if (!popup.isShowing()) {
+					popup.show(node, event.getSceneX(), event.getSceneY());
+					popup.setX(event.getSceneX() - popup.getWidth() / 2);
+					popup.setY(event.getSceneY() - popup.getHeight() - Kröw.scaleHeight(20));
+				}
+				openTransition.stop();
+				closeTransition.stop();
+				openTransition.setFromValue(openTransition.getNode().getOpacity());
+				openTransition.play();
+			}
+	
+			private void close() {
+				openTransition.stop();
+				closeTransition.stop();
+				closeTransition.setFromValue(closeTransition.getNode().getOpacity());
+				closeTransition.play();
+			}
+		};
+	
 	}
 
 }
