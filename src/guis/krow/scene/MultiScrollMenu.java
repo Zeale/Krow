3d4a5a82d28c261cd6a9592c90ec2ef32d2b8d33
Zeale <a@infinityminers.com>
2017-10-08 15:02:11 -0500

    Updated MultiScrollMenu

--- src/guis/krow/scene/MultiScrollMenu.java
+++ src/guis/krow/scene/MultiScrollMenu.java
@@ -1,5 +1,38 @@
 package krow.scene;
 
-public abstract class MultiScrollMenu extends ScrollMenu {
-	
+import java.util.List;
+
+import javafx.scene.Node;
+
+public class MultiScrollMenu extends HorizontalScrollBox {
+
+	public MultiScrollMenu() {
+	}
+
+	public class Menu {
+
+		private final VerticalScrollBox menuBox = new VerticalScrollBox();
+
+		public Menu() {
+			add();
+		}
+
+		public List<Node> getMenuItemList() {
+			return menuBox.getChildren();
+		}
+
+		public void remove() {
+			menuBox.getChildren().remove(menuBox);
+		}
+
+		public void add() {
+			getChildren().add(menuBox);
+		}
+
+		public void add(int position) {
+			getChildren().add(position, menuBox);
+		}
+
+	}
+
 }
