67b1edf00a9d9ff86be2233a2599b11d69816cb4
Zeale <a@infinityminers.com>
2017-10-08 13:27:22 -0500

    Updated HorizontalScrollControl

--- src/guis/krow/scene/HorizontalScrollControl.java
+++ src/guis/krow/scene/HorizontalScrollControl.java
@@ -9,6 +9,20 @@ import javafx.scene.layout.HBox;
 import kr�w.core.Kr�w;
 
 public class HorizontalScrollControl extends HBox {
+
+	private static final long SLIDE_ANIMATION_DURATION = 1000;
+
+	private double displacement = 0;
+
+	private static enum PropertyKeys {
+		SLIDER;
+	}
+
+	public static int NODE_WIDTH = Kr�w.scaleWidth(100), NODE_HEIGHT = Kr�w.scaleHeight(100),
+			NODE_SPACING = (int) ((double) NODE_WIDTH / 2);
+
+	private final double SINGLE_JUMP_DISTANCE = NODE_WIDTH + NODE_SPACING;
+
 	public HorizontalScrollControl() {
 		// TODO Auto-generated constructor stub
 	}
