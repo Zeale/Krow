ff687177d5fa47f8267a2ba0829cc28771d18c09
Zeale <a@infinityminers.com>
2017-10-12 00:23:05 -0500

    Added WIP LoginMessage (Chat Room)

---
+++ src/guis/krow/guis/chatroom/messages/LoginMessage.java
@@ -0,0 +1,12 @@
+package krow.guis.chatroom.messages;
+
+import kröw.connections.messages.Message;
+
+public class LoginMessage extends Message {
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
