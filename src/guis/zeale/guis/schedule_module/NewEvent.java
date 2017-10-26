052b2dbe03cb5a55a0b052dd37a1919a7274c2e0
Zeale <a@infinityminers.com>
2017-10-25 20:01:46 -0500

    Updated ScheduleModule

--- src/guis/zeale/guis/schedule_module/NewEvent.java
+++ src/guis/zeale/guis/schedule_module/NewEvent.java
@@ -1,16 +1,43 @@
 package zeale.guis.schedule_module;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import krow.guis.schedule_module.ScheduleEvent;
 import kröw.core.managers.WindowManager.Page;
 
 public class NewEvent extends Page {
 
-	NewEvent() {
+	@FXML
+	private TextField nameInput;
 
+	private ScheduleEvent event;
+
+	NewEvent() {
+		event = new ScheduleEvent();
+	}
+
+	NewEvent(ScheduleEvent event) {
+		this.event = event;
 	}
 
 	@Override
 	public String getWindowFile() {
 		return "NewEvent.fxml";
+	}
+
+	@Override
+	public void initialize() {
+		DropShadow effect = new DropShadow();
+		effect.setOffsetX(40);
+		effect.setOffsetY(30);
+		effect.setSpread(0.1);
+		effect.setRadius(0.2);
+		effect.setWidth(11.5);
+		effect.setHeight(11.5);
+		effect.setColor(new Color(0, 0, 0, 0.65));
+		nameInput.setEffect(effect);
 	}
 
 }
