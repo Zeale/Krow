a34c51021118d37cab8607d90cde6a81c6832228
Zeale <a@infinityminers.com>
2017-11-13 14:12:52 -0600

    Background Module design

--- src/guis/zeale/guis/BackgroundModule.java
+++ src/guis/zeale/guis/BackgroundModule.java
@@ -1,8 +1,13 @@
 package zeale.guis;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import kröw.core.managers.WindowManager.Page;
 
 public class BackgroundModule extends Page {
+
+	@FXML
+	private AnchorPane root, visualizerPane;
 
 	@Override
 	public String getWindowFile() {
