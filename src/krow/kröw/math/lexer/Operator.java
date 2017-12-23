152447a5b7c33a6be157c16dc76f74f5f42b5b30
Zeale <a@infinityminers.com>
2017-12-23 03:50:19 -0600

    Added Operator and Term classes
    These are currently dummy classes for development. Their fields and
    constructors may be completely changed.

---
+++ src/krow/kröw/math/lexer/Operator.java
@@ -0,0 +1,16 @@
+package kröw.math.lexer;
+
+/**
+ * Temporarily empty class for development purposes.
+ * 
+ * @author Zeale
+ *
+ */
+public class Operator {
+	private String operator;
+
+	public Operator(String operator) {
+		this.operator = operator;
+	}
+
+}
---
+++ src/krow/kröw/math/lexer/Term.java
@@ -0,0 +1,17 @@
+package kröw.math.lexer;
+
+/**
+ * Temporarily empty class for development purposes.
+ * 
+ * @author Zeale
+ *
+ */
+public class Term {
+
+	private String text;
+
+	public Term(String text) {
+		this.text = text;
+	}
+
+}
